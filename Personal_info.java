6efddb0507a32f5d270991fb355bb7364fe0a695
Keerthana0609Babu <169152616+Keerthana0609Babu@users.noreply.github.com>
2024-05-09 17:06:51 +0530

    Add files via upload

---
+++ Personal_info.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Personal_info {
+    public static void main(String[] args)
+    {
+        int Age, year;
+        Scanner object = new Scanner(System.in);
+        System.out.println("Enter the year: ");
+        year = object.nextInt();
+        Age = 2024-year;
+       object.close();
+       System.out.println("My age is: "+Age);
+       
+    
+    }
+} 
