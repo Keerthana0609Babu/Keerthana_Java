3c47e4de4d140f30d1beec1170c98271eab63a66
Keerthana0609Babu <169152616+Keerthana0609Babu@users.noreply.github.com>
2024-05-22 17:13:28 +0530

    Add files via upload

---
+++ Musicians.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Musicians {
+    public static void main(String[] args){
+
+    List<String> MusicDirectors  = new ArrayList<>();
+        MusicDirectors.add("Anirudh");
+        MusicDirectors.add("AR Rahaman");
+        MusicDirectors.add("Yuvan Shankar Raja");
+        MusicDirectors.add("Ilayaraja");
+        MusicDirectors.add("Hip Hop Thamizha");
+        for(String MusicDirector : MusicDirectors)
+            System.out.println("Check"+MusicDirector);
+        if( MusicDirector == "Anirudh")
+ 
+            System.out.println("Anirudh is a Musician.");
+            
+        
+        break;
+
+
+    
+    }
+   }
+
